10fdce347a5ed3dd3a143eb7f5e21a1e2dd710a1
Wirzaa <kmirzakerem@gmail.com>
2025-06-09 17:11:07 +0300

    Scene1

--- Graggy_Game_1/Assets/Scripts/PlayerMovement.cs
+++ Graggy_Game_1/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    private float xInput;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
+    
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
     void Update()
     {
-        
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
